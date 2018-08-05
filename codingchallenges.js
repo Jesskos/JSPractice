542f01ba07b10eef44f202a89976ab8c195efda7
Jessica Koscheka <jesskos@gmail.com>
2018-08-04 23:57:25 -0700

    codingchallenges.js

--- codingchallenges.js
+++ codingchallenges.js
@@ -15,3 +15,11 @@ function isPalindrome(phrase) { // solving without using reverse method
 
 
 }
+
+function isPermutation(str1, str2) {
+	if (str1.split("").sort().join("") === str2.split("").sort().join("")) {
+	return true } else {
+	return false 
+}
+}
+
